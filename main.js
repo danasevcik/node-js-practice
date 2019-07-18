fa4f319bec5ec2296a17e03f081bca3f90306445
Dana Sevcik <danasevcik@hotmail.com>
2019-07-18 08:49:54 -0400

    node async readFile function

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// First - test to see console log when running node
+// Console log when running node
 console.log('Hello World!');
 
 
-// Second test to see if we can create a locally hosted server
+// Create a locally hosted server
 var http = require("http");
 // console.log(http);
 
@@ -20,12 +20,12 @@ http.createServer(function (request, response) {
 // Console will print the message
 console.log('Server running at http://127.0.0.1:8081/');
 
-// Third test to require express framework
+// Require express framework
 var express = require('express');
 console.log(express);
 
 
-// Fourth test to require file system
+// Require file system
 var fs = require("fs");
 var data = fs.readFileSync('input.txt');
 
@@ -33,7 +33,7 @@ console.log(data.toString());
 console.log("Program Ended");
 
 
-// Fifth test to exemplify non-blocking
+// Exemplify non-blocking
 var fs = require("fs");
 
 fs.readFile('input.txt', function (err, data) {
@@ -41,4 +41,40 @@ fs.readFile('input.txt', function (err, data) {
    console.log(data.toString());
 });
 
+console.log("Program Ended");
+
+// Event Loop
+
+// Import events module
+var events = require('events');
+// Create an eventEmitter object
+var eventEmitter = new events.EventEmitter();
+// Create an event handler as follows
+var connectHandler = function connected() {
+   console.log('connection succesful.');
+
+   // Fire the data_received event
+   eventEmitter.emit('data_received');
+}
+// Bind the connection event with the handler
+eventEmitter.on('connection', connectHandler);
+// Bind the data_received event with the anonymous function
+eventEmitter.on('data_received', function() {
+   console.log('data received succesfully.');
+});
+// Fire the connection event
+eventEmitter.emit('connection');
+
+console.log("Program Ended.");
+
+// readFile async function
+var fs = require("fs");
+
+fs.readFile('input.txt', function (err, data) {
+   if (err) {
+      console.log(err.stack);
+      return;
+   }
+   console.log(data.toString());
+});
 console.log("Program Ended");
